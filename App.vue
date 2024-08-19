0fd642aa32d9979d6f15175817d73b263ec3cf23
casil08 <156186206+casil08@users.noreply.github.com>
2024-08-19 17:33:19 +0800

    Manage State in Vue.js

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue!</h1>
+    <TodoList />
+  </div>
+</template>
+
+<script>
+import TodoList from './TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ TodoList.vue
@@ -0,0 +1,32 @@
+ <template>
+    <div>
+     <h2>To-Do List</h2>
+     <input v-model="newItem" @keyup.enter="addItem" placeholder="Add a
+new item" />
+      <ul>
+        <li v-for="(item, index) in items" :key="index">
+          {{ item }} <button @click="removeItem(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+</template>
+</script>
+    export default {
+        data() {
+            return {
+                newItem: '',
+                items: []
+            };
+        },
+        methods: {
+            addItem() {
+                if (this.newItem !== '') {
+                    this.items.push(this.newItem);
+                    this.newItem = '';
+                }
+            },
+            removeItem(index) {
+                this.items.splice(index, 1);
+            }
+        }
+    };
